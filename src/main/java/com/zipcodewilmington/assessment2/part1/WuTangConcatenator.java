833ef403fb578ddc8abf9fa6270f452c734c0f91
Aashna <bajwa.aashna@gmail.com>
2019-06-10 12:25:04 -0400

    I hate WuTang

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -6,28 +6,28 @@ public class WuTangConcatenator {
 
     public Boolean isWu() {
         boolean multiple = false;
-        int i = 0;
-        if(i % 3 == 0)
+        Integer input = 0;
+        if(input % 3 == 0) {
             multiple = true;
-
+        }
         return multiple;
     }
 
     public Boolean isTang() {
         boolean multiple = false;
-        int i = 0;
-        if (i % 5 == 0)
+        Integer input = 0;
+        if (input % 5 == 0) {
             multiple = true;
-
+        }
         return multiple;
     }
 
     public Boolean isWuTang() {
         boolean multiple = false;
-        int i = 0;
-        if (i % 3 == 0 && i % 5 == 0)
+        Integer input = 0;
+        if (input % 3 == 0 && input % 5 == 0) {
             multiple = true;
-
+        }
         return multiple;
     }
 }
