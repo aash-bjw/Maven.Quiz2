d332f615a8361398ecf104bc2ea69123aa614bac
Aashna <bajwa.aashna@gmail.com>
2019-06-10 12:07:46 -0400

    49 tests passing

--- src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
@@ -1,4 +1,18 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class SpeedComparator {
+import java.util.Comparator;
+
+
+public class SpeedComparator{
+
 }
+
+/**implements Comparator<Animal> {
+
+    public int compare (Animal first, Animal second){
+        return first.getSpeed().compareTo(second.getSpeed());
+    }
+}
+ */
+
+
