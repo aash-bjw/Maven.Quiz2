1ca7dc8265d978ae64034534bef53da81a926080
Aashna <bajwa.aashna@gmail.com>
2019-06-16 21:49:52 -0400

    Router complete

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,20 +1,33 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+
 public class Router {
+
+
+    HashMap <String, String> outcome = new HashMap<>();
+        //this.outcome = new HashMap();
+
+
     public void add(String path, String controller) {
+        outcome.put(path, controller);
     }
 
     public Integer size() {
-        return null;
+
+        return outcome.size();
     }
 
     public String getController(String path) {
-        return null;
+
+        return outcome.get(path);
     }
 
     public void update(String path, String studentController) {
+        outcome.replace(path, studentController);
     }
 
     public void remove(String path) {
+        outcome.remove(path);
     }
 }
